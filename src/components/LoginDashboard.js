ed33d45725b4cbf3f04447892bd30e75beae6bf6
Pardo7 <walterpardo7@gmail.com>
2019-05-11 21:02:17 -0700

    refactor: Implemented login dashboard style rulesets and react logo svg

--- src/components/LoginDashboard.js
+++ src/components/LoginDashboard.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import logo from '../logo.svg';
 import { connect } from "react-redux";
 
 class LoginDashboard extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Login Below</h1>
+			<div className="card">
+				<div className="header-title">
+					<h4>Welcome To The Would You Rather App!</h4>
+					<h4 className="info-header">Please sign in to continue</h4>
+				</div>
+				<header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+				<div>
+					<h3>Sign In</h3>
+
+				</div>
 			</div>
 		);
 	}
